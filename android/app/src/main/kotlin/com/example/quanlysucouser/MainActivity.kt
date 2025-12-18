36a67f8e780c703d3c8b300817bc7c82ce6e9149
bduytuan244 <bdtuan244@gmail.com>
2025-12-18 11:26:12 +0700

    firebase

---
+++ android/app/src/main/kotlin/com/example/quanlysucouser/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.quanlysucouser
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/models/incident_model.dart
@@ -0,0 +1,51 @@
+class IncidentModel {
+  final String id;
+  final String title;
+  final String location;
+  final String description;
+  final String imageUrl;
+  final String reporterId;
+  final String? staffId;
+  final DateTime timestamp;
+  final String status;
+  final String? resultImageUrl;
+
+  IncidentModel({
+    required this.id,
+    required this.title,
+    required this.location,
+    required this.description,
+    required this.imageUrl,
+    required this.reporterId,
+    required this.timestamp,
+    this.staffId,
+    this.status = 'Pending',
+    this.resultImageUrl,
+  });
+  
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'location': location,
+      'description': description,
+      'imageUrl': imageUrl,
+      'reporterId': reporterId,
+      'timestamp': timestamp.microsecondsSinceEpoch,
+      'resultImageUrl': resultImageUrl,
+    };
+  }
+  
+  factory IncidentModel.fromMap(Map<String, dynamic> map, String documentId) {
+    return IncidentModel(
+      id: documentId,
+      title: map['title'] ?? '',
+      description: map['description'] ?? '',
+      imageUrl: map['imageUrl'] ?? '',
+      reporterId: map['reporterId'] ?? '',
+      staffId: map['staffId'],
+      timestamp: DateTime.fromMillisecondsSinceEpoch(map['timestamp'] ?? 0),
+      resultImageUrl: map['resultImageUrl'],
+    );
+  }
+}
